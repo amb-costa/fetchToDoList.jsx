5cabeab82b93d4a980fd9b2be685d1d41e3d5e82
beinganidiot <ocaranzacosta@gmail.com>
2023-03-28 22:15:32 +0000

    finished code: working on javascript, css for styling

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	//textIn: string container for text written on input
+	//textStay: array container for recording elements on input
+  let [textIn, setTextIn] = useState("");
+  let [textStay, setTextStay] = useState([]);
+
+  //input receives text, submits when pressing enter
+  //text concats to array container, erases value on input
+  //map will create <li> elements with each value
+  //trash icon will appear when li is hovered
+  //h4 will display amount of items
+  return (
+    <div id="container">
+      <h1>To-Do list!</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setTextIn(e.target.value)}
+            placeholder="What do you need to do?"
+            value={textIn}
+			onKeyDown={(e)=> {if(e.key==="Enter") {
+				setTextStay(textStay.concat(textIn));
+				setTextIn("");}}}
+          ></input>
+        </li>
+		{textStay.map((line, index) => (
+			<li key={line} id="listElement">{line}
+			<i className="fas fa-trash-alt" onClick={()=>
+			setTextStay(textStay.filter((i, current) => index!=current))}></i></li>
+		  ))}
+      </ul>
+	  <h4 style={{fontSize:20, marginTop:50}}>{(textStay.length==0)?
+		"No tasks, add a task!" :
+		textStay.length + " items"
+	  }</h4>
+    </div>
+  );
 };
 
 export default Home;
